f0d9cd093756c49ca86f78d257c7dd7aa5d78c26
lopesma11 <ml722090@gmail.com>
2025-06-23 22:14:19 -0300

    feat: creating and styling Orders Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { GlobalStyles } from "./styles/GlobalStyles";
-import { Header } from "./components/Header/Header";
+import { Header } from "./components/Header";
+import { Orders } from "./components/Orders";
 
 export function App() {
     return (
         <>
             <GlobalStyles />
             <Header />
+            <Orders />
         </>
     );
 }
